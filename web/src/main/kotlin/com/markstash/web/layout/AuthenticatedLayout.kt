0c646272c5cfaabf947f19813b72d4f91c08d55b
Daniel Rampelt <drampelt@gmail.com>
2020-07-23 20:00:29 -0400

    impr: differentiate notes and bookmarks, use pencil icon for notes

--- web/src/main/kotlin/com/markstash/web/layout/AuthenticatedLayout.kt
+++ web/src/main/kotlin/com/markstash/web/layout/AuthenticatedLayout.kt
@@ -78,7 +78,7 @@ val authenticatedLayout = functionalComponent<AuthenticatedLayoutProps> { props 
                         child(sidebarLink) {
                             attrs.label = "Notes"
                             attrs.path = "/notes"
-                            attrs.icon = "<svg fill=\"currentColor\" viewBox=\"0 0 20 20\"><path fill-rule=\"evenodd\" d=\"M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z\" clip-rule=\"evenodd\"></path></svg>"
+                            attrs.icon = "<svg fill=\"currentColor\" viewBox=\"0 0 20 20\"><path d=\"M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z\"></path></svg>"
                         }
                     }
                 }
