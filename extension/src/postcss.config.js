be82b4d7fe1df85b3f2a729951bb7bff4fdd506d
Daniel Rampelt <drampelt@gmail.com>
2020-08-20 20:41:27 -0400

    fix: correct reference to extension tailwind config

--- extension/src/postcss.config.js
+++ extension/src/postcss.config.js
@@ -6,7 +6,7 @@ module.exports = ({ options: { mode } }) => {
                 path: '../../node_modules'
             }),
             require('../../build/js/node_modules/postcss-nested'),
-            require('../../build/js/node_modules/tailwindcss')('../../../../web/src/tailwind.config.js'),
+            require('../../build/js/node_modules/tailwindcss')('../../../../extension/src/tailwind.config.js'),
             require('../../build/js/node_modules/autoprefixer'),
             mode === 'production' ? require('../../build/js/node_modules/cssnano')({preset: 'default'}) : false,
         ],
