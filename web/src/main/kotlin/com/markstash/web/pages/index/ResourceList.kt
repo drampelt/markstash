f345d077d5e3c12dd2b5971fb3c25841ba06c6e5
Daniel Rampelt <drampelt@gmail.com>
2020-09-05 12:10:04 -0400

    impr(web): show icon for bookmarks

--- web/src/main/kotlin/com/markstash/web/pages/index/ResourceList.kt
+++ web/src/main/kotlin/com/markstash/web/pages/index/ResourceList.kt
@@ -50,10 +50,14 @@ private val resourceRow = functionalComponent<ResourceRowProps> { props ->
             div("w-0 flex-grow") {
                 div("flex items-center text-sm text-gray-400") {
                     div("flex-no-shrink mr-1") {
-                        rawHtml("w-4 h-4 text-gray-700") {
-                            when (props.resource.type) {
-                                Resource.Type.BOOKMARK -> "<svg fill=\"currentColor\" viewBox=\"0 0 20 20\"><path d=\"M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z\"></path></svg>"
-                                Resource.Type.NOTE -> "<svg fill=\"currentColor\" viewBox=\"0 0 20 20\"><path d=\"M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z\"></path></svg>"
+                        if (props.resource.iconArchiveId != null) {
+                            img(src = "/api/bookmarks/${props.resource.id}/archives/${props.resource.iconArchiveId}", classes = "w-4 h-4") {}
+                        } else {
+                            rawHtml("w-4 h-4 text-gray-700") {
+                                when (props.resource.type) {
+                                    Resource.Type.BOOKMARK -> "<svg fill=\"currentColor\" viewBox=\"0 0 20 20\"><path d=\"M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z\"></path></svg>"
+                                    Resource.Type.NOTE -> "<svg fill=\"currentColor\" viewBox=\"0 0 20 20\"><path d=\"M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z\"></path></svg>"
+                                }
                             }
                         }
                     }
