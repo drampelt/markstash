acb6f28979654aebada63c6289195275023d05c4
Daniel Rampelt <drampelt@gmail.com>
2020-09-23 15:46:40 -0400

    chore(ios): setup ios project

---
+++ ios/Markstash/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Markstash
+//
+//  Created by Daniel Rampelt on 2020-09-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/Markstash/MarkstashApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkstashApp.swift
+//  Markstash
+//
+//  Created by Daniel Rampelt on 2020-09-23.
+//
+
+import SwiftUI
+
+@main
+struct MarkstashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
