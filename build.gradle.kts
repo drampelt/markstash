b3b3762b2371bce4388ad9f27b6374ceeac2ff59
Daniel Rampelt <drampelt@gmail.com>
2020-10-28 20:35:12 -0400

    chore: update AGP and gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ buildscript {
     }
 
     dependencies {
+        classpath("xml-apis:xml-apis:1.4.01") // Workaround for https://github.com/cashapp/sqldelight/issues/2058
         classpath(Dependencies.proguard)
         classpath(Dependencies.sqlDelightGradle)
         classpath(Dependencies.androidGradle)
