3d04841ad38b6d8c0c7e787b4cab39078f160634
Daniel Rampelt <drampelt@gmail.com>
2020-12-31 10:43:10 -0500

    impr(ios): support updating the observed ViewModel

--- ios/Markstash/ObservableViewModel.swift
+++ ios/Markstash/ObservableViewModel.swift
@@ -10,8 +10,9 @@ import SwiftUI
 import shared
 
 class ObservableViewModel<S, VM : StateViewModel<S>>: ObservableObject {
-    let viewModel: VM
-    @Published var state: S
+    @Published private(set) var viewModel: VM
+    @Published private(set) var state: S
+    @Published var id = UUID()
 
     init(viewModel: VM) {
         self.viewModel = viewModel
@@ -24,20 +25,37 @@ class ObservableViewModel<S, VM : StateViewModel<S>>: ObservableObject {
     func clear() {
         viewModel.clear()
     }
+
+    func replace(with newViewModel: VM) {
+        viewModel.clear()
+        viewModel = newViewModel
+        state = viewModel.currentState!
+        viewModel.observeState { newState in
+            self.state = newState!
+        }
+    }
 }
 
 @propertyWrapper struct ObservedViewModel<S, VM : StateViewModel<S>>: DynamicProperty {
-    @StateObject var viewModel: ObservableViewModel<S, VM>
+    @StateObject var observer: ObservableViewModel<S, VM>
 
     init(_ viewModel: VM) {
-        _viewModel = StateObject(wrappedValue: ObservableViewModel(viewModel: viewModel))
+        _observer = StateObject(wrappedValue: ObservableViewModel(viewModel: viewModel))
     }
 
     init(wrappedValue: VM) {
-        _viewModel = StateObject(wrappedValue: ObservableViewModel(viewModel: wrappedValue))
+        _observer = StateObject(wrappedValue: ObservableViewModel(viewModel: wrappedValue))
     }
 
-    public var wrappedValue: VM { viewModel.viewModel }
+    public var wrappedValue: VM { observer.viewModel }
 
-    public var projectedValue: S { viewModel.state }
+    public var projectedValue: S { observer.state }
+
+    func clear() {
+        observer.clear()
+    }
+
+    func replace(with newViewModel: VM) {
+        observer.replace(with: newViewModel)
+    }
 }
